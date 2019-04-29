af8ef8e07cce406c780253ee114b4147cb336965
Nonni32 <40862304+Nonni32@users.noreply.github.com>
2019-04-29 23:05:37 +0000

    vesen með portfolio klassann

--- lokaverkefni.m
+++ lokaverkefni.m
@@ -1,7 +1,19 @@
+% WEB SCRAPING DATA FROM BONDS.IS
 IndexedUrl = "http://www.bonds.is/api/market/LoadIndexed?lang=en&nonIndexed=true&nOrderbookId=-1";
-nonIndexedUrl = "http://www.bonds.is/api/market/LoadIndexed?lang=en&nonIndexed=false&nOrderbookId=-1";
-ind = webread(nonIndexedUrl);
-non = webread(IndexedUrl);
+NonIndexedUrl = "http://www.bonds.is/api/market/LoadIndexed?lang=en&nonIndexed=false&nOrderbookId=-1";
+IndexedBonds = webread(NonIndexedUrl);
+NonIndexedBonds = webread(IndexedUrl);
 
-interest = [0.038 0.0375 0.015 0.0325 0.0375 0.0375; 0.0625 0.0725 0.08 0.05 0.0625 -inf]
-coupon = [1 2 1 1 2 2; 1 1 1 1 1 -inf]
+NonIndexedPortfolio = [];
+
+for i = 1:length(IndexedBonds)
+    url = sprintf("http://www.bonds.is/api/market/LoadIndexedDetail?orderbookId=%d&lang=en",IndexedBonds(i).orderbookId);
+    tempBond = bond(IndexedBonds(i), webread(url));
+    if i == 1
+        IndexedPortfolio = portfolio(tempBond);
+    else
+        IndexedPortfolio.addToPortfolio(tempBond);
+    end
+end
+
+IndexedPortfolio
