2310873ad162d9632873296f07bf8758c8e9029e
Nonni32 <40862304+Nonni32@users.noreply.github.com>
2019-04-30 09:54:51 +0000

    ...

--- bond.m
+++ bond.m
@@ -24,15 +24,16 @@ classdef bond
             obj.lastYield = str2double(overview.lastValidYield([1:strfind(overview.lastValidYield,"%")-1]))/100;
             obj.yield = str2double(overview.yield([1:strfind(overview.yield,"%")-1]))/100;
             
-            if attributes.attributes(7).name == "coupon"
+            if attributes.attributes(7).name == "Coupon"
                 obj.coupon = attributes.attributes(7).value;
             else 
                 obj.coupon = attributes.attributes(6).value;
             end
+            
             if attributes.attributes(8).name == "Interest"
-                obj.interest = str2double(attributes.attributes(8).value([1:strfind(attributes.attributes(8).value,"%")-1]))/100;
+                obj.interest = 1.00; %str2double(attributes.attributes(8).value([1:strfind(attributes.attributes(8).value,"%")-1]))/100;
             elseif attributes.attributes(9).name == "Interest"
-                obj.interest = str2double(attributes.attributes(9).value([1:strfind(attributes.attributes(9).value,"%")-1]))/100;
+                obj.interest = 2.00; %str2double(attributes.attributes(9).value([1:strfind(attributes.attributes(9).value,"%")-1]))/100;
             end
         end
         
--- lokaverkefni.m
+++ lokaverkefni.m
@@ -7,6 +7,7 @@ NonIndexedBonds = webread(NonIndexedUrl);
 for i = 1:length(IndexedBonds)
     url = sprintf("http://www.bonds.is/api/market/LoadIndexedDetail?orderbookId=%d&lang=en", IndexedBonds(i).orderbookId);
     tempBond = bond(IndexedBonds(i), webread(url));
+
     if i == 1
         IndexedPortfolio = portfolio(tempBond);
     else
@@ -17,6 +18,7 @@ end
 for i = 1:length(NonIndexedBonds)
     url = sprintf("http://www.bonds.is/api/market/LoadIndexedDetail?orderbookId=%d&lang=en", NonIndexedBonds(i).orderbookId);
     tempBond = bond(NonIndexedBonds(i), webread(url));
+    temp = webread(url);
     if i == 1
         NonIndexedPortfolio = portfolio(tempBond);
     else
@@ -25,4 +27,4 @@ for i = 1:length(NonIndexedBonds)
 end
 
 IndexedPortfolio
-NonIndexedPortfolio
+NonIndexedPortfolio;
