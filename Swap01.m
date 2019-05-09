b0cbadd7f69ab63630b392b7e15f4350ec35ffd2
Guðmundur H. Ákason <gudmunduraka@gmail.com>
2019-05-09 17:16:53 +0000

    Meira swap

--- Swap01.m
+++ Swap01.m
@@ -25,6 +25,18 @@ swapbyzero(RateSpec, LegRate, Settle, Maturity,'LegType',LegType,...
 'AdjustCashFlowsBasis',true,...
 'BusinessDayConvention','modifiedfollow')       %'LatestFloatingRate',LatestFloatingRate,
 
+NonIndexedPortfolio = NonIndexedPortfolio.fitMethod("Swap rates","Lagrange interpolation",0,0);
+% help fitMethod
+curve = NonIndexedPortfolio.currentCurve;
+
+mdates = datenum(NonIndexedPortfolio.maturity); %Maturity dates
+dates = mdates-mdates(1)+1;                     %Dates for maturity of each bond
+Rates = curve(dates)                            %Rate at maturity of each bond
+
+plot(curve)
+hold on 
+plot(dates,Rates,'ob')
+
 
 %% Pricing Forward swap  
 clc
@@ -61,4 +73,76 @@ Price = swapbyzero(RateSpec, LegRate, Settle, Maturity, 'StartDate' , StartDate)
 LegRate = [NaN 10]; % Input
 [Price, SwapRate] = swapbyzero(RateSpec, LegRate, Settle, Maturity,...
 'StartDate' , StartDate)
+
+
+%% Swap using two interest-rate curves.
+
+%Price a swap using two interest-rate curves. 
+%First, define data for the two interest-rate term structures: 
+
+StartDates = '01-May-2012'; 
+EndDates = {'01-May-2013'; '01-May-2014';'01-May-2015';'01-May-2016'};
+Rates1 = [0.0356;0.041185;0.04489;0.047741];
+Rates2 = [0.0366;0.04218;0.04589;0.04974];
+
+RateSpecReceiving = intenvset('Rates', Rates1, 'StartDates',StartDates,...
+'EndDates', EndDates, 'Compounding', 1);
+
+RateSpecPaying= intenvset('Rates', Rates2, 'StartDates',StartDates,...
+'EndDates', EndDates, 'Compounding', 1);
+
+RateSpec=[RateSpecReceiving RateSpecPaying]
+
+%Define swap instruments
+
+Settle = '01-May-2012';
+Maturity = '01-May-2015';
+LegRate = [0.06 10]; 
+Principal = [100;50;100];
+
+%Price three swaps using the two curves. 
+
+Price = swapbyzero(RateSpec, LegRate, Settle, Maturity, 'Principal', Principal)
+
+%% Price a Swap Using a Different Curve to Generate the Cash Flows of the Floating Leg
+
+Settle = datenum('15-Mar-2013');
+CurveDates = daysadd(Settle,360*[1/12 2/12 3/12 6/12 1 2 3 4 5 7 10],1);
+OISRates = [.0018 .0019 .0021 .0023 .0031 .006  .011 .017 .021 .026 .03]';
+LiborRates = [.0045 .0047 .005 .0055 .0075 .011 .016 .022 .026 .030 .0348]';
+
+%Plot the dual curves.
+
+figure,plot(CurveDates,OISRates,'r');hold on;plot(CurveDates,LiborRates,'b')
+datetick
+legend({'OIS Curve', 'Libor Curve'})
+
+Create an associated RateSpec for the OIS and Libor curves.
+
+OISCurve = intenvset('Rates',OISRates,'StartDate',Settle,'EndDates',CurveDates);
+LiborCurve = intenvset('Rates',LiborRates,'StartDate',Settle,'EndDates',CurveDates);
+
+%Define the swap.
+
+Maturity = datenum('15-Mar-2018'); % Five year swap
+FloatSpread = 0;
+FixedRate = .025;
+LegRate = [FixedRate FloatSpread];
+
+%Compute the price of the swap instrument. The 
+%LiborCurve term structure will be used to generate the cash 
+%flows of the floating leg. The OISCurve term structure will 
+%be used for discounting the cash flows.
+
+Price = swapbyzero(OISCurve, LegRate, Settle,...
+Maturity,'ProjectionCurve',LiborCurve)
+
+
+%Compare results when the term structure OISCurve is used both 
+%for discounting and also generating the cash flows of the floating leg.
+
+PriceSwap = swapbyzero(OISCurve, LegRate, Settle, Maturity)
+
+
+
 
